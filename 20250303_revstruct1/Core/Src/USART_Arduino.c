a4e10a0fd2ffe6d7fbf7eb16dae1fa3f16ef0209
s2301089 <s2301089@sendai-nct.jp>
2025-03-04 09:51:03 +0900

    yu-sen pre

--- 20250303_revstruct1/Core/Src/USART_Arduino.c
+++ 20250303_revstruct1/Core/Src/USART_Arduino.c
@@ -14,12 +14,15 @@ int getData(UART_HandleTypeDef* huart,getdata* revdata){
 	if(RevAF(huart) == 0xaf){
 		AddArray(huart,Rdata);
 		if(ChSUM(Rdata) == 0xed){
+//			printf("chsum ed ");
 			AddStruct(revdata,Rdata);
 			HAL_UART_Abort(huart);
 			memset(Rdata,0x00,sizeof(Rdata));
 			return 0;
 		}
 	}
+	HAL_UART_Abort(huart);
+	memset(Rdata,0x00,sizeof(Rdata));
 	return -1;
 }
 
@@ -32,14 +35,14 @@ uint8_t RevAF(UART_HandleTypeDef* huart){
 		if(Gdata == 0xaf){
 			return Gdata;
 		}else{
-			printf("not 0xaf\r\n");
+//			printf("not 0xaf %02x\r\n",Gdata);
 			return 0x00;
 		}
 	}else {
 		timeout++;
-		printf("else %2d\r\n",timeout);
+//		printf("else %2d\r\n",timeout);
 		if(timeout >= TIMEOUT_MAX){
-			printf("timeout\r\n");
+//			printf("timeout");
 			timeout = 0;
 		}
 		return 0xff;
@@ -59,6 +62,7 @@ void AddArray(UART_HandleTypeDef* huart,uint8_t* Adata){
 			i--;
 		}
 	}
+//	OutPut(Adata);
 
 	return;
 }
@@ -72,11 +76,11 @@ uint8_t ChSUM(uint8_t* Adata){
 		if(revsum == Adata[9]){
 			return 0xed;
 		}else {
-			printf("sum is not true\r\n");
+//			printf("sum is not true\r\n");
 			return 0x00;
 		}
 	}else {
-		printf("not end");
+//		printf("not end ");
 		return 0xff;
 	}
 }
@@ -90,23 +94,23 @@ void AddStruct(getdata* Udata,uint8_t* Adata){
 	Udata->RY = Adata[4];
 	Udata->R2 = Adata[6];
 
-	if(Adata[6] & 0x01)	Udata->TRIANGLE = 1;
-	if(Adata[6] & 0x02)	Udata->CIRCLE 	= 1;
-	if(Adata[6] & 0x04)	Udata->CROSS 	= 1;
-	if(Adata[6] & 0x08)	Udata->SQUARE 	= 1;
-	if(Adata[6] & 0x10)	Udata->UP 		= 1;
-	if(Adata[6] & 0x20)	Udata->RIGHT 	= 1;
-	if(Adata[6] & 0x40)	Udata->DOWN 	= 1;
-	if(Adata[6] & 0x80)	Udata->LEFT		= 1;
+	if(Adata[7] & 0x01)	Udata->TRIANGLE = 1;
+	if(Adata[7] & 0x02)	Udata->CIRCLE 	= 1;
+	if(Adata[7] & 0x04)	Udata->CROSS 	= 1;
+	if(Adata[7] & 0x08)	Udata->SQUARE 	= 1;
+	if(Adata[7] & 0x10)	Udata->UP 		= 1;
+	if(Adata[7] & 0x20)	Udata->RIGHT 	= 1;
+	if(Adata[7] & 0x40)	Udata->DOWN 	= 1;
+	if(Adata[7] & 0x80)	Udata->LEFT		= 1;
 
-	if(Adata[7] & 0x01)	Udata->L1 		= 1;
-	if(Adata[7] & 0x02)	Udata->L3		= 1;
-	if(Adata[7] & 0x04)	Udata->R1		= 1;
-	if(Adata[7] & 0x08)	Udata->R3		= 1;
-	if(Adata[7] & 0x10)	Udata->SHARE 	= 1;
-	if(Adata[7] & 0x20)	Udata->OPTION 	= 1;
-	if(Adata[7] & 0x40)	Udata->PS 		= 1;
-	if(Adata[7] & 0x80)	Udata->TOUCHPAD = 1;
+	if(Adata[8] & 0x01)	Udata->L1 		= 1;
+	if(Adata[8] & 0x02)	Udata->L3		= 1;
+	if(Adata[8] & 0x04)	Udata->R1		= 1;
+	if(Adata[8] & 0x08)	Udata->R3		= 1;
+	if(Adata[8] & 0x10)	Udata->SHARE 	= 1;
+	if(Adata[8] & 0x20)	Udata->OPTION 	= 1;
+	if(Adata[8] & 0x40)	Udata->PS 		= 1;
+	if(Adata[8] & 0x80)	Udata->TOUCHPAD = 1;
 
 	return;
 }
@@ -137,4 +141,96 @@ void StructInit(getdata* Udata){
 	Udata->TOUCHPAD = 0;
 
 	return;
+}
+
+//void OutPut(uint8_t* Adata){
+//	for(int i = 0;i < REV_SIZE;i++){
+//		printf("%2d %02x ",i,Adata[i]);
+//	}
+//}
+
+void AllShow(getdata Udata){
+	printf("LX:%3d "
+			"LY:%3d "
+			"L2:%3d "
+			"RX:%3d "
+			"RY:%3d "
+			"R2:%3d "
+			"TRIANGLE:%d "
+			"CIRCLE:%d "
+			"CROSS:%d "
+			"SQUARE:%d "
+			"UP:%d "
+			"RIGHT:%d "
+			"DOWN:%d "
+			"LEFT:%d "
+			"L1:%d "
+			"L3:%d "
+			"R1:%d "
+			"R3:%d "
+			"SHARE:%d "
+			"OPTION:%d "
+			"PS:%d "
+			"TOUCHPAD:%d"
+			"\r\n"
+			,Udata.LX
+			,Udata.LY
+			,Udata.L2
+			,Udata.RX
+			,Udata.RY
+			,Udata.R2
+			,Udata.TRIANGLE
+			,Udata.CIRCLE
+			,Udata.CROSS
+			,Udata.SQUARE
+			,Udata.UP
+			,Udata.RIGHT
+			,Udata.DOWN
+			,Udata.LEFT
+			,Udata.L1
+			,Udata.L3
+			,Udata.R1
+			,Udata.R3
+			,Udata.SHARE
+			,Udata.OPTION
+			,Udata.PS
+			,Udata.TOUCHPAD
+			);
+}
+
+void AllShowP(getdata Udata){
+	printf(
+			"LX:%3d "
+			"LY:%3d "
+			"RX:%3d "
+			"RY:%3d "
+			"L2:%3d "
+			"R2:%3d "
+			"Pressed Button: "
+			,Udata.LX
+			,Udata.LY
+			,Udata.RX
+			,Udata.RY
+			,Udata.L2
+			,Udata.R2
+			);
+
+	if(Udata.TRIANGLE	== 1) printf("TRIANGLE ");
+	if(Udata.CIRCLE		== 1) printf("CIRCLE ");
+	if(Udata.CROSS		== 1) printf("CROSS ");
+	if(Udata.SQUARE 	== 1) printf("SQUARE ");
+	if(Udata.UP 		== 1) printf("UP ");
+	if(Udata.RIGHT 		== 1) printf("RIGHT ");
+	if(Udata.DOWN 		== 1) printf("DOWN ");
+	if(Udata.LEFT 		== 1) printf("LEFT ");
+	if(Udata.L1 		== 1) printf("L1 ");
+	if(Udata.L3 		== 1) printf("L3 ");
+	if(Udata.R1 		== 1) printf("R1 ");
+	if(Udata.R3 		== 1) printf("R3 ");
+	if(Udata.SHARE 		== 1) printf("SHARE ");
+	if(Udata.OPTION 	== 1) printf("OPTION ");
+	if(Udata.PS 		== 1) printf("PS ");
+	if(Udata.TOUCHPAD 	== 1) printf("TOUOCHPAD ");
+
+	printf("\r\n");
 }
--- 20250303_revstruct1/Core/Src/main.c
+++ 20250303_revstruct1/Core/Src/main.c
@@ -43,6 +43,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
@@ -53,6 +54,7 @@ UART_HandleTypeDef huart2;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -93,6 +95,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
   getdata data;
 
@@ -105,8 +108,10 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  printf("snct\r\n");
-	  HAL_Delay(500);
+//	  getData(&huart1,&data);
+	  if(getData(&huart1,&data) == 0){
+		  AllShowP(data);
+	  }
   }
   /* USER CODE END 3 */
 }
@@ -150,6 +155,39 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 38400;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
 }
 
 /**
