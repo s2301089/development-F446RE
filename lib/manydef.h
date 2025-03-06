f76344494a9271c5f588317609f9d9788ca87336
s2301089 <s2301089@sendai-nct.jp>
2025-03-06 09:24:42 +0900

    add uartv2

--- lib/manydef.h
+++ lib/manydef.h
@@ -46,4 +46,41 @@
 #define PG		GPIOG
 #define PH		GPIOH
 
+#define	GPIO_Pin_0		0x00000001
+#define	GPIO_Pin_1		0x00000002
+#define GPIO_Pin_2		0x00000004
+#define GPIO_Pin_3		0x00000008
+
+#define GPIO_Pin_4		0x00000010
+#define GPIO_Pin_5		0x00000020
+#define GPIO_Pin_6		0x00000040
+#define GPIO_Pin_7		0x00000080
+
+#define GPIO_Pin_8		0x00000100
+#define GPIO_Pin_9		0x00000200
+#define GPIO_Pin_10		0x00000400
+#define GPIO_Pin_11		0x00000800
+
+#define GPIO_Pin_12 	0x00001000
+#define GPIO_Pin_13 	0x00002000
+#define GPIO_Pin_14 	0x00004000
+#define GPIO_Pin_15 	0x00008000
+
+#define GPIO_Pin_All	0x0000ffff
+
+#define GPIOxA GPIOA
+#define GPIOxB GPIOB
+#define GPIOxC GPIOC
+#define GPIOxD GPIOD
+#define GPIOxE GPIOE
+#define GPIOxF GPIOF
+#define GPIOxG GPIOG
+#define GPIOxH GPIOH
+
+#define TIMCH_1			0x00000000U
+#define TIMCH_2			0x00000004U
+#define TIMCH_3			0x00000008U
+#define TIMCH_4			0x0000000CU
+#define TIMCH_ALL		0x0000003CU
+
 #endif /* INC_MANYDEF_H_ */
