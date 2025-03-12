50f60452d2d593ed9ab12f1b236d242804d74640
s2301089 <s2301089@sendai-nct.jp>
2025-03-12 12:01:37 +0900

    plus suc

--- 20250312_meka9/Core/Src/mekanamu.c
+++ 20250312_meka9/Core/Src/mekanamu.c
@@ -41,73 +41,56 @@ signed char halfY(uint8_t Y){
 
 void MoveST(uint8_t *Udata,uint8_t stX,uint8_t stY,uint8_t stV){ // Udata[0] 左上 Udata[1] 右上 Udata[2] 左下 Udata[3] 右下
 #define PLUS 45
-	signed char PosX;
-	signed char PosY;
-	double Adata[8] = {0};
+	double PosX;
+	double PosY;
+	double Vroll;
 	double Z;
-	double theta;
-	double theta_plus;
 	double COS;
 	double SIN;
-
+	double theta;
+	double theta_plus;
+	double Adata[8];
 	PosX = halfX(stX);
 	PosY = halfY(stY);
+	Vroll = halfY(stV) * RAITO;
 	Z = sqrt(pow(PosX,2) + pow(PosY,2));
 	theta = atan2(PosY,PosX);
-//	theta_plus = theta - rad(PLUS);
-//	theta_plus = theta;
-//	COS = Z * cos(theta_plus);
-//	SIN = Z * sin(theta_plus);
-//
-//	double Vroll;
-//	Vroll = (double)halfY(stV) * RAITO;
-//
-//	Adata[LUP] = COS + SIN - Vroll;
-//	Adata[RUP] = - COS - SIN - Vroll;
-//	Adata[LDP] = - COS + SIN - Vroll;
-//	Adata[RDP] = COS - SIN - Vroll;
+	theta_plus = rad(PLUS);
 
-	theta_plus = theta - rad(PLUS);
-	COS = Z * cos(theta_plus);
-	SIN = Z * sin(theta_plus);
-	double Vroll;
-	Vroll = halfY(stV) * RAITO;
-	Adata[LUP] = SIN + COS + Vroll;
-	Adata[RUP] = SIN + COS + Vroll;
-	Adata[LDP] = SIN + COS + Vroll;
-	Adata[RDP] = SIN + COS + Vroll;
-//	Adata[LUP] = -COS;
-//	Adata[RUP] = SIN;
-//	Adata[LDP] = -SIN;
-//	Adata[RDP] = COS;
+	COS = (Z * cos(theta)) / 2;
+	SIN = (Z * sin(theta)) / (2 * sin(theta_plus) * cos(theta_plus));
+//	for(int i = 0;i < 4;i++){
+//		SIN[i] = (Z * sin(theta)) / (2 * sin(theta_plus + (360 / 4) * i) * cos(theta_plus + (360 / 4) * i));
+//	}
+
+	Adata[LUP] = - COS - SIN - Vroll;
+	Adata[RUP] =   COS + SIN - Vroll;
+	Adata[LDP] =   COS - SIN - Vroll;
+	Adata[RDP] = - COS + SIN - Vroll;
+	printf("SIN %f COS %f ",SIN,COS);
 
 	for(int i = 0;i < 4;i++){
-		if(Adata[i+4] < 0){
-			if(Adata[i+4] < -0x7f){
-				Adata[i+4] = -0x7f;
+		if(Adata[i + 4] < 0){
+			if(Adata[i + 4] < -0x7f){
+				Adata[i + 4] = -0x7f;
 			}
 			Udata[i] = 1;
-			Udata[i+4] = (uint8_t)(0x7f + Adata[i+4]) * 2;
+			Udata[i + 4] = (uint8_t)(0x7f + Adata[i + 4]) * 2;
 			printf("nega ");
 		}else{
-			if(Adata[i+4] > 0x7f){
-				Adata[i+4] = 0x7f;
+			if(Adata[i + 4] > 0x7f){
+				Adata[i + 4] = 0x7f;
 			}
 			Udata[i] = 0;
-			Udata[i+4] = (uint8_t)Adata[i+4] * 2;
+			Udata[i + 4] = (uint8_t)(Adata[i + 4]) * 2;
 			printf("posi ");
 		}
 	}
 
-//	Udata[LUD] = !Udata[LUD];
-//	Udata[LDD] = !Udata[LDD];
 //	Udata[LUP] = 0xff - Udata[LUP];
+//	Udata[LUD] = !Udata[LUD];
 //	Udata[LDP] = 0xff - Udata[LDP];
-
-//	printf("左上 %d %2X 右上 %d %2X 左下 %d %2X 右下 %d %2X %f\r\n",Udata[LUD],Udata[LUP],Udata[RUD],Udata[RUP],Udata[LDD],Udata[LDP],Udata[RDD],Udata[RDP],Vroll);
-//	printf("左上 %d %+4f 右上 %d %+4f 左下 %d %+4f 右下 %d %+4f %f\r\n",Udata[LUD],Adata[LUP],Udata[RUD],Adata[RUP],Udata[LDD],Adata[LDP],Udata[RDD],Adata[RDP],Vroll);
-//	printf("%f %f %f %f\r\n",COS,SIN,theta,deg(theta));
-	printf("%f %f ",deg(theta),deg(theta_plus));
+//	Udata[LDD] = !Udata[LDD];
 }
 
 
