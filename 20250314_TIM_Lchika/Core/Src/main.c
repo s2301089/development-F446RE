d0e99f71a80cf2b25cc857ed34e11d15dc987076
s2301089 <s2301089@sendai-nct.jp>
2025-03-14 17:00:48 +0900

    a

--- 20250314_TIM_Lchika/Core/Src/main.c
+++ 20250314_TIM_Lchika/Core/Src/main.c
@@ -164,9 +164,9 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 16000;
+  htim2.Init.Prescaler = 15;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 1000;
+  htim2.Init.Period = 999999;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
